53884752bf652c10258c8e7f71d7dbfead8db27a
ngmcong <ngmcong@outlook.com>
2024-12-29 20:07:15 +0700

    *

---
+++ src/ObjectCommon.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System;
+
+namespace phantom
+{
+    public static class ObjectExtention
+    {
+        /// <summary>Clone một đối tượng.</summary>
+        /// Changes:
+        /// - 210120: 1.0.0.0 [CNM]: Khởi tạo.
+        /// <param name="input">Đối tượng cần clone.</param>
+        /// <returns>Đối tượng mới clone.</returns>
+        public static T DeepCopy<T>(this T input) where T : class
+        {
+            T outObj = Activator.CreateInstance<T>();
+            var properties = typeof(T).GetProperties().Where(x => x.CanWrite);
+            foreach (var property in properties)
+            {
+                property.SetValue(outObj, property.GetValue(input, null), null);
+            }
+            return outObj;
+        }
+    }
+}
